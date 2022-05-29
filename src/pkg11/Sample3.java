aca7aeb4e7f169d4170ad7d78ab83716ccfae126
murgeshnb <mnbechetti@gmail.com>
2022-05-29 14:12:20 +0530

    user for conflict

---
+++ src/pkg11/Sample3.java
@@ -0,0 +1,12 @@
+package pkg11;
+
+public class Sample3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("umain user comment");
+
+	}
+
+}
