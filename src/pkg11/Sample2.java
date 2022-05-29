b58db11c51cf331607ba2eedb26ce44766b51d48
murgeshnb <mnbechetti@gmail.com>
2022-05-29 14:17:21 +0530

    dcjjnksdfj

--- src/pkg11/Sample2.java
+++ src/pkg11/Sample2.java
@@ -5,7 +5,7 @@ public class Sample2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("usr2 comment");
+		System.out.println("usr2 comment.....");
 
 	}
 
